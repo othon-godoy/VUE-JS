6e2fd198dee27bfbd64cad376550c48bb7007be6
Othon Godoy <othon.dev@outlook.com>
2020-01-03 01:03:43 -0300

    VUE JS

---
+++ COMPONENTES/comunicacao-componentes/src/components/UsuarioInfo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do usuário <strong> {{ idade }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar nome</button>
+        <button @click="reiniciarFn()">Reiniciar nome (Callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            //required: true,
+            type: String,
+            default: 'Anônimo'
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            const antigo = this.nome
+
+            this.nome = 'Pedro',
+            //this.$emit('nomeMudou', this.nome)
+            this.$emit('nomeMudou', {
+                novo: this.nome,
+                antigo: this.antigo
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
