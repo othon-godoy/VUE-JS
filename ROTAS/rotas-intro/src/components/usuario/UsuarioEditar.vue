c20130b510419b31d5f51b472f509481beaff060
Othon Godoy <othon.dev@outlook.com>
2020-01-23 21:24:27 -0300

    VUE JS

---
+++ ROTAS/rotas-intro/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="usuario-editar">
+        <h3>Usuário Editar</h3>
+        <p><strong>Código:</strong>&nbsp;{{ id }}</p>
+        <p><strong>Completo:</strong>&nbsp;{{ $route.query.completo ? 'Sim' : 'Não' }}</p>
+        <p><strong>Língua:</strong>&nbsp;{{ $route.query.lingua }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+	props: ['id'],
+}
+</script>
+
+<style>
+	
+</style>
